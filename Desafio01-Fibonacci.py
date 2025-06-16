77ae5cb33078f9c8711c1cb77df1c8d23081b0dd
Tonitzpp <134986282+Tonitzpp@users.noreply.github.com>
2025-06-16 14:27:51 -0300

    Add files via upload

--- Desafio01-Fibonacci.py
+++ Desafio01-Fibonacci.py
@@ -24,19 +24,31 @@ def Fibonacci_linear(n):
     else:
         return v0
 
-
 def main():
-    try:
+    
+    while True:
+        try:
+            n = int(input("Digite um número inteiro (N >= 0): "))
 
-        n = int(input("Digite um número inteiro (N >= 0): "))
+            print("\nResultado: ")
+            print(f"Fibonacci Recursivo de {n}: {Fibonacci_recursivo(n)}")
+            print(f"Fibonacci Linear de {n}: {Fibonacci_linear(n)}")
 
-        print("\nResultado: ")
-        print(f"Fibonacci Recursivo de {n}: {Fibonacci_recursivo(n)}")
-        print(f"Fibonacci Linear de {n}: {Fibonacci_linear(n)}")
+            # coloquei esta opção de resposta para facilitar novas verificações, 
+            # assim o usuário não terá que compilar o programa novamente
+            # caso queira verificar um novo número.
+            while True:
+                resp = input("\nDeseja verificar mais um número (S/N)?").strip().upper()
+                if resp in ("S", "N"):
+                    break
+                print("Resposta inválida. Digite apenas 'S' ou 'N'.")
 
+            if resp == "N":
+                print("Encerrando programa...")
+                break
 
-    except ValueError as e:
-        print("Erro: ", e)
+        except ValueError as e:
+            print("Erro: ", e)
 
 if __name__ == "__main__":
     main()
