197450b4d6979ff27ae0cc0b7788d65b00cf324d
Tonitzpp <134986282+Tonitzpp@users.noreply.github.com>
2025-06-16 14:26:42 -0300

    Add files via upload

---
+++ Desafio01-Fibonacci.py
@@ -0,0 +1,42 @@
+
+def Fibonacci_recursivo(n):
+    if n < 0:
+        raise ValueError("O valor de N deve ser maior ou igual a 0.")
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    return Fibonacci_recursivo(n - 1) + Fibonacci_recursivo(n - 2)
+
+def Fibonacci_linear(n):
+    if n < 0:
+        raise ValueError("O valor de N deve ser maior ou igual a 0.")
+    if n == 0:
+        return 0
+    
+    v0 = 0
+    v1 = 1
+    for _ in range(n - 1):
+        v0, v1 = v1, v0 + v1
+    if n > 0:
+        return v1
+    else:
+        return v0
+
+
+def main():
+    try:
+
+        n = int(input("Digite um número inteiro (N >= 0): "))
+
+        print("\nResultado: ")
+        print(f"Fibonacci Recursivo de {n}: {Fibonacci_recursivo(n)}")
+        print(f"Fibonacci Linear de {n}: {Fibonacci_linear(n)}")
+
+
+    except ValueError as e:
+        print("Erro: ", e)
+
+if __name__ == "__main__":
+    main()
